02c6cf6f84cc8b786c712ce5af8d5d275b151ffa
Muhamad Aziz <muhamad17042002@email.com>
2026-07-25 02:09:00 +0700

    fix: change default port to 8080 for Railway

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { validateConfig } = require('./src/config/config');
 const Logger = require('./src/utils/logger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
